157a1aa137180e9ee1fdaa926dd5b01ffec3e358
ElirPereza <jeuzmaldo2020@gmail.com>
2024-07-04 13:07:28 -0500

    update pull request
    fixing hydratation error on the toggleTheme

--- src/Components/darkModeButton.tsx
+++ src/Components/darkModeButton.tsx
@@ -1,13 +1,21 @@
 "use client"
-import { IconSvgProps } from "./Icons"
+import { useEffect, useState } from "react";
+import { IconSvgProps, MoonIcon, SunIcon } from "./Icons"
 import { useTheme } from "next-themes"
 
 export default function DarkModeButton() {
     const {theme, setTheme} = useTheme()
-
+    const [mounted, setMounted] = useState(false);
+    console.log(theme)
+    useEffect(() => {
+      setMounted(true);
+    }, []);
+  
     const toggleTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
-      };
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+  
+    if (!mounted) return null; 
 
   return (
     <div
@@ -33,49 +41,3 @@ export default function DarkModeButton() {
   )
 }
 
-function MoonIcon(props:IconSvgProps) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-    </svg>
-  )
-}
-
-
-function SunIcon(props:IconSvgProps) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="4" />
-      <path d="M12 2v2" />
-      <path d="M12 20v2" />
-      <path d="m4.93 4.93 1.41 1.41" />
-      <path d="m17.66 17.66 1.41 1.41" />
-      <path d="M2 12h2" />
-      <path d="M20 12h2" />
-      <path d="m6.34 17.66-1.41 1.41" />
-      <path d="m19.07 4.93-1.41 1.41" />
-    </svg>
-  )
-}
